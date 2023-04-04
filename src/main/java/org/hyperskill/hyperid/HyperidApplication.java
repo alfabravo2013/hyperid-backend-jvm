ed37f1b94403db02f10a5c3a327d4df7515ad082
Viktor Martynov <2013alfabravo@gmail.com>
2023-04-04 09:42:51 +0400

    initial setup

---
+++ src/main/java/org/hyperskill/hyperid/HyperidApplication.java
@@ -0,0 +1,13 @@
+package org.hyperskill.hyperid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hyperskill/hyperid/HyperidApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyperskill.hyperid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
